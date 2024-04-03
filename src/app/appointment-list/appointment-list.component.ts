9774e2e33fa0c5ae2e1305d7e50bcf0cb2299ad9
charan <charan2ravi@gmail.com>
2024-04-03 09:52:46 +0530

    added localStorage and bootstrap css

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Appointment } from '../models/appointment';
 
 @Component({
@@ -6,6 +6,37 @@ import { Appointment } from '../models/appointment';
   templateUrl: './appointment-list.component.html',
   styleUrl: './appointment-list.component.css',
 })
-export class AppointmentListComponent {
-  appointment: Appointment[] = [];
+export class AppointmentListComponent implements OnInit {
+  newAppointmentTitle: string = '';
+  newAppointmentDate: Date = new Date();
+  appointments: Appointment[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    let savedAppointments = localStorage.getItem('appointments');
+    this.appointments = savedAppointments ? JSON.parse(savedAppointments) : [];
+  }
+
+  addAppointment() {
+    if (this.newAppointmentTitle.trim().length && this.newAppointmentDate) {
+      let newAppointment: Appointment = {
+        id: Date.now(),
+        title: this.newAppointmentTitle,
+        date: this.newAppointmentDate,
+      };
+
+      this.appointments.push(newAppointment);
+      this.newAppointmentTitle = '';
+      this.newAppointmentDate = new Date();
+
+      localStorage.setItem('appointments', JSON.stringify(this.appointments));
+      console.log(newAppointment);
+    }
+  }
+
+  deleteAppointment(index: number) {
+    this.appointments.splice(index, 1);
+    localStorage.setItem('appointments', JSON.stringify(this.appointments));
+  }
 }

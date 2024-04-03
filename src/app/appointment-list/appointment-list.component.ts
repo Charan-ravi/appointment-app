7e31b1c35c04cd8b7069fdc25bb38a921e0d64bd
charan <charan2ravi@gmail.com>
2024-04-03 08:29:56 +0530

    Initial commit

---
+++ src/app/appointment-list/appointment-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Appointment } from '../models/appointment';
+
+@Component({
+  selector: 'app-appointment-list',
+  templateUrl: './appointment-list.component.html',
+  styleUrl: './appointment-list.component.css',
+})
+export class AppointmentListComponent {
+  appointment: Appointment[] = [];
+}
